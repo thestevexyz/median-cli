038fc96db0f59e9a006b1c755faaed9923bc7c86
Steven <steven@thesteve.xyz>
2022-03-05 13:50:01 +0100

    Finished working program

--- main.go
+++ main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
 func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		printErrorAndExit("No numbers were provided")
+	}
+
+	var argsParsed = []float64{}
+	for _, arg := range args {
+		float, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			printErrorAndExit(fmt.Sprintf("Failed parsing argument '%s' to float64", arg))
+		}
+
+		argsParsed = append(argsParsed, float)
+	}
+
+	fmt.Println(calculateMedian(argsParsed))
+}
+
+func calculateMedian(numbers []float64) float64 {
+	sort.Float64s(numbers)
+
+	middleNumber := len(numbers) / 2
+
+	if len(numbers)%2 != 0 {
+		return numbers[middleNumber]
+	}
+
+	return (numbers[middleNumber-1] + numbers[middleNumber]) / 2
+}
+
+func printErrorAndExit(message string) {
+	fmt.Printf("Error: %s\n", message)
+	os.Exit(1)
 }
